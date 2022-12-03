04c57e5315e45778db4fadca7ff3a08e986d772d
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-12-03 14:31:03 -0500

    Add initial code

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<div>
+			<p>Simon</p>
+		</div>
+	);
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
